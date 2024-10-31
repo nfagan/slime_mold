58a5ebf5dc42200c4c2f30ff8fbc005ac898e4a7
Nick Fagan <nick@fagan.org>
2024-10-30 22:19:25 -0400

    simplify main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,3 @@
-// Dear ImGui: standalone example application for Emscripten, using GLFW + WebGPU
-// (Emscripten is a C++-to-javascript compiler, used to publish executables for the web. See https://emscripten.org/)
-
-// Learn about Dear ImGui:
-// - FAQ                  https://dearimgui.com/faq
-// - Getting Started      https://dearimgui.com/getting-started
-// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
-// - Introduction, links and more at the top of imgui.cpp
-
 #include "SoilComponent.hpp"
 #include "SoilGUI.hpp"
 #include "wgpu_imshow.hpp"
@@ -22,7 +13,6 @@
 #include <webgpu/webgpu.h>
 #include <webgpu/webgpu_cpp.h>
 
-// Global WebGPU required states
 static WGPUDevice        wgpu_device = nullptr;
 static WGPUSurface       wgpu_surface = nullptr;
 static WGPUTextureFormat wgpu_preferred_fmt = WGPUTextureFormat_RGBA8Unorm;
@@ -30,15 +20,10 @@ static WGPUSwapChain     wgpu_swap_chain = nullptr;
 static int               wgpu_swap_chain_width = 0;
 static int               wgpu_swap_chain_height = 0;
 
-// Forward declarations
 static void MainLoopStep(void* window);
 static bool InitWGPU();
 static void print_glfw_error(int error, const char* description);
 static void print_wgpu_error(WGPUErrorType error_type, const char* message, void*);
-
-/*
- * My routines + data
- */
 
 namespace {
 
@@ -48,42 +33,59 @@ struct {
   bool full_screen_image{};
 } globals;
 
-void do_my_init() {
+void main_init() {
   globals.soil.initialize();
 }
 
-float do_my_update() {
+float main_update() {
   return globals.soil.update();
 }
 
-void do_my_begin_frame() {
-  wgpu::begin_frame({
-    wgpu_device,
-    wgpu_preferred_fmt,
-    globals.use_bw,
-    globals.full_screen_image,
-    wgpu_swap_chain_width,
-    wgpu_swap_chain_height,
-    globals.soil.get_texture_dim()
-  }, globals.soil.get_soil()->read_rgbau8_image_data());
-}
+void main_gui_update(float sim_dt) {
+  const float fps = ImGui::GetIO().Framerate;
 
-void do_my_render_pass(WGPURenderPassEncoder enc) {
-  wgpu::draw_image(enc);
-}
+  ImGui_ImplWGPU_NewFrame();
+  ImGui_ImplGlfw_NewFrame();
+  ImGui::NewFrame();
 
-void do_my_gui_update(float fps, float sim_dt) {
   auto res = SoilGUI::render(globals.soil, fps, sim_dt, &globals.use_bw, &globals.full_screen_image);
   globals.soil.on_gui_update(res);
 }
 
+void main_render() {
+  ImGui::Render();
+
+  static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+  WGPURenderPassColorAttachment color_attachments = {};
+  color_attachments.loadOp = WGPULoadOp_Clear;
+  color_attachments.storeOp = WGPUStoreOp_Store;
+  color_attachments.clearValue = { clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w };
+  color_attachments.view = wgpuSwapChainGetCurrentTextureView(wgpu_swap_chain);
+  WGPURenderPassDescriptor render_pass_desc = {};
+  render_pass_desc.colorAttachmentCount = 1;
+  render_pass_desc.colorAttachments = &color_attachments;
+  render_pass_desc.depthStencilAttachment = nullptr;
+
+  WGPUCommandEncoderDescriptor enc_desc = {};
+  WGPUCommandEncoder encoder = wgpuDeviceCreateCommandEncoder(wgpu_device, &enc_desc);
+
+  WGPURenderPassEncoder pass = wgpuCommandEncoderBeginRenderPass(encoder, &render_pass_desc);
+
+  //  my renderer
+  wgpu::draw_image(pass);
+
+  ImGui_ImplWGPU_RenderDrawData(ImGui::GetDrawData(), pass);
+  wgpuRenderPassEncoderEnd(pass);
+
+  WGPUCommandBufferDescriptor cmd_buffer_desc = {};
+  WGPUCommandBuffer cmd_buffer = wgpuCommandEncoderFinish(encoder, &cmd_buffer_desc);
+  WGPUQueue queue = wgpuDeviceGetQueue(wgpu_device);
+  wgpuQueueSubmit(queue, 1, &cmd_buffer);
+}
+
 } //  anon
 
-/*
-
-https://github.com/ocornut/imgui/issues/6640
-
-*/
+//  https://github.com/ocornut/imgui/issues/6640
 EM_JS(int, browser_get_width, (), {
   const { width, height } = canvas.getBoundingClientRect();
   return width;
@@ -99,35 +101,27 @@ static void get_window_dimensions(int* dw, int* dh) {
   *dh = browser_get_height();
 }
 
-/*
- * End my routines
- */
-
-// Main code
 int main(int, char**)
 {
   glfwSetErrorCallback(print_glfw_error);
-  if (!glfwInit())
+  if (!glfwInit()) {
     return 1;
+  }
 
-  // Make sure GLFW does not initialize any graphics context.
-  // This needs to be done explicitly later.
   glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+WebGPU example", nullptr, nullptr);
-  if (!window)
-  {
+  GLFWwindow* window = glfwCreateWindow(1280, 720, "SlimeMold", nullptr, nullptr);
+  if (!window) {
     glfwTerminate();
     return 1;
   }
 
   // Initialize the WebGPU environment
-  if (!InitWGPU())
-  {
-    if (window)
-      glfwDestroyWindow(window);
+  if (!InitWGPU()) {
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 1;
   }
+
   glfwShowWindow(window);
 
   // Setup Dear ImGui context
@@ -136,54 +130,22 @@ int main(int, char**)
   ImGuiIO& io = ImGui::GetIO(); (void)io;
   io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
   io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-
-  // For an Emscripten build we are disabling file-system access, so let's not attempt to do a fopen() of the imgui.ini file.
-  // You may manually call LoadIniSettingsFromMemory() to load settings from your own storage.
   io.IniFilename = nullptr;
-
-  // Setup Dear ImGui style
   ImGui::StyleColorsDark();
-  //ImGui::StyleColorsLight();
-
   // Setup Platform/Renderer backends
   ImGui_ImplGlfw_InitForOther(window, true);
   ImGui_ImplWGPU_Init(wgpu_device, 3, wgpu_preferred_fmt, WGPUTextureFormat_Undefined);
-
-  // Load Fonts
-  // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-  // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-  // - If the file cannot be loaded, the function will return a nullptr. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-  // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-  // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
-  // - Read 'docs/FONTS.md' for more instructions and details.
-  // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-  // - Emscripten allows preloading a file or folder to be accessible at runtime. See Makefile for details.
-  //io.Fonts->AddFontDefault();
 #ifndef IMGUI_DISABLE_FILE_FUNCTIONS
-  //io.Fonts->AddFontFromFileTTF("fonts/segoeui.ttf", 18.0f);
   io.Fonts->AddFontFromFileTTF("fonts/DroidSans.ttf", 16.0f);
-  //io.Fonts->AddFontFromFileTTF("fonts/Roboto-Medium.ttf", 16.0f);
-  //io.Fonts->AddFontFromFileTTF("fonts/Cousine-Regular.ttf", 15.0f);
-  //io.Fonts->AddFontFromFileTTF("fonts/ProggyTiny.ttf", 10.0f);
-  //ImFont* font = io.Fonts->AddFontFromFileTTF("fonts/ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
-  //IM_ASSERT(font != nullptr);
 #endif
 
   /*
-   * My init
+   * init
    */
 
-  do_my_init();
+  main_init();
 
-  /*
-   * End my init
-   */
-
-  // This function will directly return and exit the main function.
-  // Make sure that no required objects get cleaned up.
-  // This way we can use the browsers 'requestAnimationFrame' to control the rendering.
   emscripten_set_main_loop_arg(MainLoopStep, window, 0, false);
-
   return 0;
 }
 
@@ -213,28 +175,13 @@ static bool InitWGPU()
   return true;
 }
 
-static void MainLoopStep(void* window)
-{
-  /*
-   * My update
-   */
-
-  const float sim_dt = do_my_update();
-
-  /*
-   * End update
-   */
-
-  ImGuiIO& io = ImGui::GetIO();
-
-  glfwPollEvents();
-
+void main_begin_frame(GLFWwindow* window) {
   int width, height;
   glfwGetFramebufferSize((GLFWwindow*)window, &width, &height);
 
   int window_width;
   int window_height;
-  glfwGetWindowSize((GLFWwindow*)window, &window_width, &window_height);
+  glfwGetWindowSize(window, &window_width, &window_height);
 
   int browser_w;
   int browser_h;
@@ -262,93 +209,42 @@ static void MainLoopStep(void* window)
     ImGui_ImplWGPU_CreateDeviceObjects();
   }
 
-  do_my_begin_frame();
+  wgpu::begin_frame({
+    wgpu_device,
+    wgpu_preferred_fmt,
+    globals.use_bw,
+    globals.full_screen_image,
+    wgpu_swap_chain_width,
+    wgpu_swap_chain_height,
+    globals.soil.get_texture_dim()
+  }, globals.soil.get_soil()->read_rgbau8_image_data());
+}
 
-  // Start the Dear ImGui frame
-  ImGui_ImplWGPU_NewFrame();
-  ImGui_ImplGlfw_NewFrame();
-  ImGui::NewFrame();
-
+static void MainLoopStep(void* window)
+{
   /*
-   * My render
+   * update
    */
 
-  do_my_gui_update(io.Framerate, sim_dt);
+  glfwPollEvents();
+  const float sim_dt = main_update();
 
   /*
-   * End my render
+   * begin frame
    */
 
-  // Our state
-  // (we use static, which essentially makes the variable globals, as a convenience to keep the example code easy to follow)
-  static bool show_demo_window = true;
-  static bool show_another_window = false;
-  static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+  main_begin_frame((GLFWwindow*) window);
 
-  // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-#if 0
-  if (show_demo_window)
-    ImGui::ShowDemoWindow(&show_demo_window);
+  /*
+   * gui update
+   */
 
-  // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
-  {
-    static float f = 0.0f;
-    static int counter = 0;
+  main_gui_update(sim_dt);
 
-    ImGui::Begin("Hello, world!");                                // Create a window called "Hello, world!" and append into it.
-
-    ImGui::Text("This is some useful text.");                     // Display some text (you can use a format strings too)
-    ImGui::Checkbox("Demo Window", &show_demo_window);            // Edit bools storing our window open/close state
-    ImGui::Checkbox("Another Window", &show_another_window);
-
-    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);                  // Edit 1 float using a slider from 0.0f to 1.0f
-    ImGui::ColorEdit3("clear color", (float*)&clear_color);       // Edit 3 floats representing a color
-
-    if (ImGui::Button("Button"))                                  // Buttons return true when clicked (most widgets return true when edited/activated)
-      counter++;
-    ImGui::SameLine();
-    ImGui::Text("counter = %d", counter);
-
-    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-    ImGui::End();
-  }
-#endif
-
-  // 3. Show another simple window.
-  if (show_another_window)
-  {
-    ImGui::Begin("Another Window", &show_another_window);         // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-    ImGui::Text("Hello from another window!");
-    if (ImGui::Button("Close Me"))
-      show_another_window = false;
-    ImGui::End();
-  }
-
-  // Rendering
-  ImGui::Render();
-
-  WGPURenderPassColorAttachment color_attachments = {};
-  color_attachments.loadOp = WGPULoadOp_Clear;
-  color_attachments.storeOp = WGPUStoreOp_Store;
-  color_attachments.clearValue = { clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w };
-  color_attachments.view = wgpuSwapChainGetCurrentTextureView(wgpu_swap_chain);
-  WGPURenderPassDescriptor render_pass_desc = {};
-  render_pass_desc.colorAttachmentCount = 1;
-  render_pass_desc.colorAttachments = &color_attachments;
-  render_pass_desc.depthStencilAttachment = nullptr;
-
-  WGPUCommandEncoderDescriptor enc_desc = {};
-  WGPUCommandEncoder encoder = wgpuDeviceCreateCommandEncoder(wgpu_device, &enc_desc);
-
-  WGPURenderPassEncoder pass = wgpuCommandEncoderBeginRenderPass(encoder, &render_pass_desc);
-  do_my_render_pass(pass);
-  ImGui_ImplWGPU_RenderDrawData(ImGui::GetDrawData(), pass);
-  wgpuRenderPassEncoderEnd(pass);
-
-  WGPUCommandBufferDescriptor cmd_buffer_desc = {};
-  WGPUCommandBuffer cmd_buffer = wgpuCommandEncoderFinish(encoder, &cmd_buffer_desc);
-  WGPUQueue queue = wgpuDeviceGetQueue(wgpu_device);
-  wgpuQueueSubmit(queue, 1, &cmd_buffer);
+  /*
+   * gpu render pass
+   */
+  main_render();
 }
 
 static void print_glfw_error(int error, const char* description)
