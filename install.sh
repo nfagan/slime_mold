7497f211af564a1cc9d4a3b7bafc3480ca8feda8
Nick Fagan <nick@fagan.org>
2025-01-09 09:25:02 -0500

    deploy wgl

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 cp cmake-build-web/slime_mold.js .
 cp cmake-build-web/slime_mold.data .
 cp cmake-build-web/slime_mold.wasm .
+cp cmake-build-web/slime_mold_wgl.js .
+cp cmake-build-web/slime_mold_wgl.data .
+cp cmake-build-web/slime_mold_wgl.wasm .
+
